2babddd12b747f90334bea393e12a8ae975fa9c2
RubenMelkumyan20 <melkumyan.realru.ruben@mail.ru>
2016-08-26 12:27:09 +0400

    Add files via upload

---
+++ im sayt/The smartest robot/my.js
@@ -0,0 +1,9 @@
+var showAnswer=function() {
+    var hiddenDiv= document.getElementById("showAnswer");
+	var answer=document.getElementById("answer");
+	var password=document.getElementById("password").value;
+	answer.innerHTML=password;
+	document.getElementById("questionDiv").style.display="none";
+	hiddenDiv.style.display="block";
+	document.getElementById("text").style.display="none";
+};
